55010a4a49e56faa9ed72f41d7acb0f796b23fe9
Lama-Abdullah1 <443200881@student.ksu.edu.sa>
2024-03-28 18:09:11 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
 
 import 'dart:ui';
@@ -9,7 +8,6 @@ import 'package:flutter_application_1/MyFlight.dart';
 import 'package:flutter_application_1/login.dart';
 import 'package:flutter_application_1/signUp.dart';
 
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +15,7 @@ import 'package:flutter/widgets.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    // Replace with your actual Firebase project configuration
+  // Replace with your actual Firebase project configuration
   final firebaseConfig = FirebaseOptions(
     apiKey: "AIzaSyAspkUJ_FrWCRiIZ_gq8EXbAB3yNUUw7WI",
     appId: "1:519520418458:android:377e038a9eabbf58debd23",
@@ -25,10 +23,10 @@ Future<void> main() async {
     projectId: "lightflight-7abd4",
   );
   await Firebase.initializeApp(options: firebaseConfig);
-  
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyFlight(),
+    home: welcomPage(),
     theme: ThemeData(
       primaryColor: Color(0xFF2F83C5),
       primaryColorLight: Color(0xFF1BAEC6),
@@ -45,136 +43,115 @@ Future<void> main() async {
   ));
 }
 
-
-
-class welcomPage extends StatelessWidget{
+class welcomPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-return Scaffold(
-  backgroundColor: Color(0xFFF3F9FB),
-  body: SafeArea(
-    
-    child: Container(
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height,
-      padding: EdgeInsets.symmetric(horizontal: 30,vertical: 50),
-      child: Column(
-
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-   
-          
-          Column(children: <Widget>[
-            SizedBox(height: 20,),
-                   Container(
-                    
-              height: MediaQuery.of(context).size.height/3,
-              decoration: BoxDecoration(
-                image:DecorationImage(image: AssetImage("images/lightFlightLogo.png"),
-                ),
-
+    return Scaffold(
+      backgroundColor: Color(0xFFF3F9FB),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/lightFlightLogo.png"),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Text(
+                    "Welcom to LightFlight",
+                    style: TextStyle(
+                      color: Color(0xFF096499),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Where Your Journey Begins!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700], //mayby change
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
-
-            ),
-            
-            SizedBox(height: 60,),
-                         
-            Text("Welcom to LightFlight" ,
-            style: TextStyle(
-              color: Color(0xFF096499),
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Where Your Journey Begins!",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.grey[700], //mayby change
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-              
-
-
-            ),),
-
-
-
-
-          ],),
-
-
-     
-            Column(
-              children: <Widget>[
-                //login btn
-                MaterialButton(
-
-
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder:(context)=>loginPage()));
-                  },
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.black
+              Column(
+                children: <Widget>[
+                  //login btn
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => loginPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(50),
                     ),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Text("Login",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-
-                  ),),
-
-                ),
-
-                //signup btn
-                SizedBox(height: 20,),
-                MaterialButton(
-                  
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder:(context)=>signupPage()));
-                  },
-                  color: Color.fromARGB(255, 104, 204, 220),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Color(0xFF1BAEC6),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(50),
                   ),
-                  child: Text("SignUp",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
 
-                  ),),
-                ),
-              ],
-
-          
-            ),
-
-
-
-        ],
-
-
+                  //signup btn
+                  SizedBox(
+                    height: 20,
+                  ),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => signupPage()));
+                    },
+                    color: Color.fromARGB(255, 104, 204, 220),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Color(0xFF1BAEC6),
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Text(
+                      "SignUp",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      
-    
-
-    ),
-  ),
-);
-
-}
+    );
+  }
 }
