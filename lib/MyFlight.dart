a90a0e37626bbb6c553ab4b9778293ab881582c4
Aljawhara Mansour Abdullah <144268551+Aljawhara-Aldhayan-443200433@users.noreply.github.com>
2024-04-19 10:30:02 +0300

    Edit flight details.

--- lib/MyFlight.dart
+++ lib/MyFlight.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'Account.dart';
 import 'recommendations.dart';
 import 'FlightBookingHomePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,13 +30,29 @@ class _ExpansionListState extends State<MyFlight> {
   String myEmail = '';
   late List<Item> _data;
   bool _isLoading = true;
-  
+
+  TextEditingController departure= new TextEditingController();
+  TextEditingController destination= new TextEditingController();
+  TextEditingController departureDate= new TextEditingController();
+  TextEditingController departuretime= new TextEditingController();
+  TextEditingController arrivaTime= new TextEditingController();
+  TextEditingController bookingClass= new TextEditingController();
+  late List<String> departureAndDestinations;
+
+  List<String> bookingclasses=['First Class','Economy','Business','Premium Economy'];
+  Future<void> fetchAndStoreFlights() async {
+    departureAndDestinations= await getFlightArray(); 
+  }
+
+  String? ChoosedClass;
+  String? ChoosedFlight;
   
 
   @override
   void initState() {
     super.initState();
     _fetch();
+    fetchAndStoreFlights();
   }
 
   _fetch() async {
@@ -57,7 +76,7 @@ class _ExpansionListState extends State<MyFlight> {
         final airlineID = airlineRef != null ? airlineRef.id : '';
       //search for the airline with this id
         final airlineDoc = await FirebaseFirestore.instance.collection('Airline').doc(airlineID).get();
-        print('Available airline Data: ${airlineDoc.data()}');
+        print('Available airline info: ${airlineDoc.data()}');
         if (airlineDoc.exists) {
         //get available flight id
           final availableFlightRef = airlineDoc.data()?['AvaliableFlightID'];
@@ -65,7 +84,7 @@ class _ExpansionListState extends State<MyFlight> {
         //search for the available flight with this id
           final availableFlightDoc = await FirebaseFirestore.instance.collection('AvaliableFlight').doc(availableFlightID).get();
           if (availableFlightDoc.exists) {
-            print('Available Flight Data: ${availableFlightDoc.data()}');
+            print('Available Flight info: ${availableFlightDoc.data()}');
 
         final classRef= reservationDoc.data()?['classID'];
         final classId=  classRef !=null ? classRef.id: '';
@@ -78,10 +97,19 @@ class _ExpansionListState extends State<MyFlight> {
         final seatDoc= await FirebaseFirestore.instance.collection('Seat').doc(seatId).get();
         print ('seat info: ${seatDoc.data()}') ; 
 
+        List<String> timeSplit = availableFlightDoc.data()?['DepartureTime'].split(':');
+        int DepartureHour = int.parse(timeSplit[0]);
+        int DepartureMinute = int.parse(timeSplit[1]);
 
+
+        List<String> timeSplit2 = availableFlightDoc.data()?['ArrivalTime'].split(':');
+        int arrivalHour = int.parse(timeSplit2[0]);
+        int arrivalMinute = int.parse(timeSplit2[1]);
 
             Item item = Item(
               reservationID: reservationDoc.id,
+              AirlineID: airlineID,
+              availableFlightid: availableFlightID,
               logo: 'images/'+ (airlineDoc.data()?['logo'] ?? ''),
               destination: availableFlightDoc.data()?['Destenation'] ?? '',
               departure: availableFlightDoc.data()?['Departure'] ?? '',
@@ -90,14 +118,16 @@ class _ExpansionListState extends State<MyFlight> {
               seatNum: seatDoc.id ?? '',
               departureDate: availableFlightDoc.data()?['DepartureDate'] ?? '',
               bookingClass: classDoc.data()?['Type'] ?? '',
+              timeOfDayDeparure: TimeOfDay(hour: DepartureHour, minute: DepartureMinute),
+              timeOfDayArrival: TimeOfDay(hour:arrivalHour, minute:arrivalMinute),
             );
 
             items.add(item); 
           } else {
-            print('Available Flight document not found for ID: $availableFlightID');
+            print('AvailableFlight  is not for: $availableFlightID');
           }
         } else {
-          print('Airline document not found for ID: $airlineID');
+          print('Airline is not found for: $airlineID');
         }
       }
       setState(() {
@@ -139,7 +169,7 @@ class _ExpansionListState extends State<MyFlight> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text("From:", style: Theme.of(context).textTheme.bodyLarge),
-                          Text(item.destination, style: Theme.of(context).textTheme.bodyMedium),
+                          Text(item.departure, style: Theme.of(context).textTheme.bodyMedium),
                         ],
                       ),
                       SizedBox(width: 100),
@@ -148,7 +178,7 @@ class _ExpansionListState extends State<MyFlight> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text("To:", style: Theme.of(context).textTheme.bodyLarge),
-                            Text(item.departure, style: Theme.of(context).textTheme.bodyMedium),
+                            Text(item.destination, style: Theme.of(context).textTheme.bodyMedium),
                           ],
                         ),
                       ),
@@ -157,7 +187,12 @@ class _ExpansionListState extends State<MyFlight> {
                 ),
                 children: [
                   ListTile(
-                    leading: Icon(Icons.edit),
+                    leading: GestureDetector(
+                      onTap:(){
+                    EditFlightDetail(item);
+                      } ,
+                      child:Icon(Icons.edit),
+                      ),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -180,7 +215,7 @@ class _ExpansionListState extends State<MyFlight> {
                         Text("Palace of Versailles", style: Theme.of(context).textTheme.bodySmall),
                       ],
                     ),
-         trailing: IconButton(
+        trailing: IconButton(
       onPressed: () {
        // Show dialog to confirm deletion
   showDialog(
@@ -221,7 +256,7 @@ class _ExpansionListState extends State<MyFlight> {
                  
                   ),
                 ],
-                
+ 
                 initiallyExpanded: item.isExpanded,
               );
             }).toList(),
@@ -308,9 +343,332 @@ class _ExpansionListState extends State<MyFlight> {
       );
     }
   }
+
+Future<void> EditFlightDetail(Item item) async {
+  String initialChoosedFlight = 'From:${item.departure} To:${item.destination}';
+  String initialChoosedClass = item.bookingClass;
+  departuretime =TextEditingController(text:item.departureTime);
+  arrivaTime=TextEditingController(text:item.arrivalTime);
+  departureDate=TextEditingController(text:item.departureDate);
+
+  await showDialog(
+    context: context,
+    builder: (context) => StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                DropdownButton<String>(
+                  hint: Text('Change flights'),
+                  value: initialChoosedFlight,
+                  isExpanded: true,
+                  onChanged: (newValue) {
+                    if (newValue != null && newValue != initialChoosedFlight) {
+                      showConfirmationDialog(
+                        context,
+                        "Confirm Flight Change!!",
+                        "Are you sure you want to change the flight?",
+                        () {
+                          setState(() {
+                            initialChoosedFlight = newValue;
+                          });
+                          _UpdateAvailableFlight(newValue, item);
+                        }
+                      );
+                    }
+                  },
+                  items: departureAndDestinations.map((valueItem) {
+                    return DropdownMenuItem(
+                      value: valueItem,
+                      child: Text(valueItem),
+                    );
+                  }).toList(),
+                ),
+                DropdownButton<String>(
+                  hint: Text('Update the booking class'),
+                  value: initialChoosedClass,
+                  isExpanded: true,
+                  onChanged: (newValue) {
+                    if (newValue != null && newValue != initialChoosedClass) {
+                      showConfirmationDialog(
+                        context,
+                        "Confirm Booking Class Change!!",
+                        "Are you sure you want to change the booking class?",
+                        () {
+                          setState(() {
+                            initialChoosedClass = newValue;
+                          });
+                          _UpdateBokkingClass(newValue, item);
+                        }
+                      );
+                    }
+                  },
+                  items: bookingclasses.map((valueItem) {
+                    return DropdownMenuItem(
+                      value: valueItem,
+                      child: Text(valueItem),
+                    );
+                  }).toList(),
+                ),
+                TextField(
+                  controller: departureDate,
+                  decoration: InputDecoration(
+                    labelText: 'Departure date',
+                    filled: true,
+                    prefixIcon: Icon(Icons.calendar_today),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Theme.of(context).primaryColorDark),
+                    ),
+                  ),
+                  readOnly: true,
+                  onTap: () => _selectDate(item),
+                ),
+                TextField(
+                  controller: departuretime,
+                  decoration: InputDecoration(labelText: 'Departure time'),
+                  readOnly: true,
+                  onTap: () => _showTimePickerDeparture(item),
+                ),
+                TextField(
+                  controller: arrivaTime,
+                  decoration: InputDecoration(labelText: 'Arrival time'),
+                  readOnly: true,
+                  onTap: () => _showTimePickerArrival(item),
+                ),
+              ],
+            ),
+          ),
+          title: Center(child: Text("Edit Flight", style: Theme.of(context).textTheme.bodyMedium)),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    ),
+  );
 }
 
+
+void showConfirmationDialog(BuildContext context, String title, String content, Function onConfirm) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: <Widget>[
+          TextButton(
+            child: Text('No'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          TextButton(
+            child: Text('Yes'),
+            onPressed: () {
+              onConfirm();
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+
+
+
+
+    Future<void> _selectDate(Item item) async{
+      DateTime initialDate = DateTime.tryParse(item.departureDate) ?? DateTime.now();
+      DateTime? _picked = await showDatePicker(
+        context:context,
+        initialDate: initialDate,
+        firstDate:DateTime(2024),
+        lastDate:DateTime(2100),
+      );
+      if(_picked !=null){
+        String formattedDate = DateFormat('yyyy/MM/dd').format(_picked);
+        setState(() {
+          departureDate.text= formattedDate;
+          item.departureDate=formattedDate;
+        });
+        await FirebaseFirestore.instance.collection('AvaliableFlight').doc(item.availableFlightid).update({
+          'DepartureDate': formattedDate,
+         });
+      }
+    }
+    Future<void> _showTimePickerDeparture(Item item) async{
+      TimeOfDay? pickedTime= await showTimePicker(
+        context: context,
+       initialTime: item.timeOfDayDeparure
+       );
+
+       if(pickedTime!=null){
+        setState(() {
+          item.timeOfDayDeparure=pickedTime;  
+          String formattedHour1 = pickedTime.hour.toString().padLeft(2, '0');
+          String formattedMinute1 = pickedTime.minute.toString().padLeft(2, '0');
+          item.departureTime='$formattedHour1:$formattedMinute1';
+          departuretime.text = item.departureTime;
+        });
+
+
+        String formattedHour = pickedTime.hour.toString().padLeft(2, '0');
+        String formattedMinute = pickedTime.minute.toString().padLeft(2, '0');
+        String storeNewTime = '$formattedHour:$formattedMinute';
+
+        await FirebaseFirestore.instance.collection('AvaliableFlight').doc(item.availableFlightid).update({
+          'DepartureTime': storeNewTime,
+         });   
+
+         
+       }
+      
+
+    }
+
+    Future<void> _showTimePickerArrival(Item item) async{
+      TimeOfDay? pickedTime= await showTimePicker(
+        context: context,
+       initialTime: item.timeOfDayArrival
+       );
+
+       if(pickedTime!=null){
+        setState(() {
+          item.timeOfDayArrival=pickedTime;  
+          String formattedHour1 = pickedTime.hour.toString().padLeft(2, '0');
+          String formattedMinute1 = pickedTime.minute.toString().padLeft(2, '0');
+          item.arrivalTime='$formattedHour1:$formattedMinute1';
+          arrivaTime.text = item.arrivalTime;
+        });
+
+
+        String formattedHour = pickedTime.hour.toString().padLeft(2, '0');
+        String formattedMinute = pickedTime.minute.toString().padLeft(2, '0');
+        String storeNewTime = '$formattedHour:$formattedMinute';
+
+        await FirebaseFirestore.instance.collection('AvaliableFlight').doc(item.availableFlightid).update({
+          'DepartureTime': storeNewTime,
+         });   
+
+         
+       }
+      
+
+    }
+
+  
+Future<void> _UpdateAvailableFlight(String newValue, Item item) async{
+
+ var bySpace=newValue.split(" To:");
+ String departure=bySpace[0].replaceFirst("From:", "");
+ String destination=bySpace[1];
+
+ if(departure.isNotEmpty && destination.isNotEmpty){
+
+  QuerySnapshot changeFlights= await FirebaseFirestore.instance.collection('AvaliableFlight').where('Departure', isEqualTo:departure).where('Destenation', isEqualTo:destination).get();
+    var newFlight=changeFlights.docs.first;
+  print(newFlight.data());
+  if(changeFlights.docs.isNotEmpty){
+    QuerySnapshot changeAirline= await FirebaseFirestore.instance.collection('Airline').where('AvaliableFlightID', isEqualTo:newFlight.reference).get();
+    if(changeAirline.docs.isNotEmpty){
+      var newAirline=changeAirline.docs.first;
+    print(newAirline.data());
+      await  FirebaseFirestore.instance.collection('Reservation').doc(item.reservationID).update({
+          'airlineID': newAirline.reference,
+         }); 
+       print("Airline/${newAirline.id}");
+       print("New Flight Reference: ${ newAirline.reference.path}");
+       print(newFlight.id);
+       String documentId = newFlight.id.toString();
+       print(documentId);
+       DocumentSnapshot newFlightDetails = await FirebaseFirestore.instance.collection('AvaliableFlight').doc(documentId).get();
+       print(newFlightDetails.data());
+       DocumentSnapshot newAirline2 = await FirebaseFirestore.instance.collection('Airline').doc(newAirline.id).get();
+       print(newAirline2.data());
+      Map<String, dynamic> newAirlineData = newAirline2.data() as Map<String, dynamic>;
+      Map<String, dynamic> newFlightData = newFlightDetails.data() as Map<String, dynamic>;
+
+        setState(() {
+           item.AirlineID="Airline/${newAirline.id}";
+           item.logo='images/'+(newAirlineData['logo']);
+           item.departure=newFlightData['Departure'];
+           item.destination=newFlightData['Destenation'];
+           item.arrivalTime=newFlightData['ArrivalTime'];
+           item.departureTime=newFlightData['DepartureTime'];
+           item.departureDate=newFlightData['DepartureDate'];
+             
+
+           ChoosedFlight = 'From:${departure} To:${destination}';
+         }); 
+    }
+
+
+
+
+  }
+
+
+ }
+}
+
+Future<void> _UpdateBokkingClass(String newValue, Item item) async{
+
+
+  Map<String, String> classMapping = {
+  'Economy': 'class2',
+  'Business': 'class3',
+  'First Class': 'class1',
+  'Premium Economy': 'class4',
+};
+
+ String? classID=classMapping[newValue];
+
+if(classID!=null){
+  DocumentReference classRef = FirebaseFirestore.instance.collection('Class').doc(classID);
+  print(classRef);
+   FirebaseFirestore.instance.collection('Reservation').doc(item.reservationID).update({
+          'classID': classRef,
+         });  
+
+   setState(() {
+          item.bookingClass=newValue;
+ });       
+}
+
+}
+
+}
+
+Future<List<String>> getFlightArray() async{
+
+ List<String> DepartureToDestination=[];
+
+  final availableFlightDoc= await FirebaseFirestore.instance.collection('AvaliableFlight');
+
+QuerySnapshot flights= await availableFlightDoc.get() ;
+  for(var availableFlight in flights.docs){
+    Map<String, dynamic> flightData=availableFlight.data() as Map<String, dynamic>;
+    String departure=flightData?['Departure'] ?? '';
+    String destination=flightData?['Destenation'] ?? '';
+    DepartureToDestination.add('From:$departure To:$destination');
+    
+  }
+  return DepartureToDestination;
+}
+
+
 class Item {
+  String reservationID;
+  String AirlineID;
+  String availableFlightid;
   String logo;
   String destination;
   String departure;
@@ -319,10 +677,13 @@ class Item {
   String seatNum;
   String departureDate;
   String bookingClass;
+  TimeOfDay timeOfDayDeparure;
+  TimeOfDay timeOfDayArrival;
   bool isExpanded;
-  String reservationID;
- 
   Item({
+    required this.reservationID,
+    required this.AirlineID,
+    required this.availableFlightid,
     required this.logo,
     required this.destination,
     required this.departure,
@@ -331,8 +692,8 @@ class Item {
     required this.seatNum,
     required this.departureDate,
     required this.bookingClass,
-    required this.reservationID,
-    this.isExpanded = false, 
-   
+    required this.timeOfDayDeparure,
+    required this.timeOfDayArrival,
+    this.isExpanded = false,
   });
 }
